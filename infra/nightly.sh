d87a70853a1563547758a199fd6d32921a047b88
Anjali <anjalip@cs.washington.edu>
2025-12-11 14:40:44 -0800

    minimal nightly for flamegraph

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -10,4 +10,12 @@ export PATH=~/.cargo/bin:$PATH
 rustup update
 
 cargo build --release
-perf record -F 999 --call-graph dwarf -- ./target/release/poach tests/repro-unsound.egg out sequential-round-trip ; perf script --demangle | rustfilt | ../FlameGraph/stackcollapse-perf.pl | ../FlameGraph/flamegraph.pl > graph.svg
+perf record -F 999 --call-graph dwarf -- ./target/release/poach tests/repro-unsound.egg out sequential-round-trip ; perf script --demangle | rustfilt | ../FlameGraph/stackcollapse-perf.pl | ../FlameGraph/flamegraph.pl > flamegraph.svg
+
+rm -rf nightly
+
+mkdir -p nightly/output
+
+cp infra/nightly-resources/web/* nightly/output
+
+cp flamegraph.svg nightly/output
